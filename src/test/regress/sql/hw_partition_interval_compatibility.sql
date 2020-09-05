26d23328bfc0637423ff692ceb9ab76be72afa93
yujiang <wutheringwind@163.com>
2020-09-05 18:22:46 +0800

    modify interval partition testcase
    Signed-off-by: yujiang <wutheringwind@163.com>

--- src/test/regress/sql/hw_partition_interval_compatibility.sql
+++ src/test/regress/sql/hw_partition_interval_compatibility.sql
@@ -15,7 +15,7 @@ INTERVAL ('1 day')
 	PARTITION p1 VALUES LESS THAN (('2020-03-01'))
 );
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 insert into interval_tab1 values(1,'2020-2-29',  1, 1);
 insert into interval_tab1 values(1,'2020-3-1',  1, 1);
@@ -28,7 +28,7 @@ insert into interval_tab1 values(1,'2020-3-7',  1, 1);
 insert into interval_tab1 values(1,'2020-4-2',  1, 1);
 
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 select * from interval_tab1 where logdate >= '2020-3-1' and logdate < '2020-3-7';
 
@@ -36,11 +36,11 @@ explain (costs off, verbose on, nodes off) select * from interval_tab1 where log
 
 alter table interval_tab1 drop partition p1;
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 alter table interval_tab1 merge partitions sys_p5, sys_p6 into partition sys_p5_p6;
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 drop table interval_tab1;
 
@@ -62,13 +62,13 @@ insert into interval_tab1 values(1,'2020-3-1 02:00:00',  1, 1);
 insert into interval_tab1 values(1,'2020-3-2',  1, 1);
 insert into interval_tab1 values(1,'2020-4-2',  1, 1);
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 select * from interval_tab1 partition(sys_p1);
 
 alter table interval_tab1 split partition sys_p1 at (to_date('2020-03-02', 'YYYY-MM-DD')) into (partition sys_p1_1, partition sys_p1_2);
 
-select relname, parttype, parentid, partstrategy, boundaries from pg_partition;
+select relname, parttype, partstrategy, boundaries from pg_partition;
 
 drop table interval_tab1;
 
