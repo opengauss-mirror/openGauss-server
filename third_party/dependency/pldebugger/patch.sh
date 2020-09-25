3bf833c64ad17c104f63da3d3130e66f4e75cd2e
sqyyeah <948885883@qq.com>
2020-09-25 17:08:20 +0800

    rename pldebugger patch and fix lineno display problem

--- third_party/dependency/pldebugger/patch.sh
+++ third_party/dependency/pldebugger/patch.sh
@@ -1,7 +1,7 @@
 rm -rf pldebugger_3_0
 tar xfzv pldebugger_3_0.tar.gz &> /dev/null
 rename ".c" ".cpp" pldebugger_3_0/*.c
-file_name="huawei_pldebugger.patch"
+file_name="pldebugger_3_0_patch.patch"
 if [ ! -f "$file_name" ]; then 
 	exit 0; 
 fi
