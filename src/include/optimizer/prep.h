27ee02c1f17a2e5fe66ddbf871bad660684c6eb1
zhouxiongjia <719216473@qq.com>
2020-09-30 15:32:55 +0800

    rewrite ROWNUM to LIMIT if possible

--- src/include/optimizer/prep.h
+++ src/include/optimizer/prep.h
@@ -19,6 +19,8 @@
 #include "nodes/relation.h"
 
 #include "utils/guc.h"
+#include "catalog/pg_operator.h"
+#include "optimizer/clauses.h"
 
 /*
  * prototypes for prepjointree.c
@@ -26,51 +28,108 @@
 extern void pull_up_sublinks(PlannerInfo* root);
 extern void substitute_ctes_with_subqueries(PlannerInfo* root, Query* parse, bool under_recursive_tree);
 extern void inline_set_returning_functions(PlannerInfo* root);
-extern Node* pull_up_subqueries(
-    PlannerInfo* root, Node* jtnode, JoinExpr* lowest_outer_join, AppendRelInfo* containing_appendrel);
-extern void flatten_simple_union_all(PlannerInfo* root);
-extern void removeNotNullTest(PlannerInfo* root);
-extern void reduce_outer_joins(PlannerInfo* root);
-extern void reduce_inequality_fulljoins(PlannerInfo* root);
-extern Relids get_relids_in_jointree(Node* jtnode, bool include_joins);
-extern Relids get_relids_for_join(PlannerInfo* root, int joinrelid);
-extern void pull_up_subquery_hint(PlannerInfo* root, Query* parse, HintState* hint_state);
+extern Node *pull_up_subqueries(PlannerInfo *root, Node *jtnode, JoinExpr *lowest_outer_join,
+    AppendRelInfo *containing_appendrel);
+extern void flatten_simple_union_all(PlannerInfo *root);
+extern void removeNotNullTest(PlannerInfo *root);
+extern void reduce_outer_joins(PlannerInfo *root);
+extern void reduce_inequality_fulljoins(PlannerInfo *root);
+extern Relids get_relids_in_jointree(Node *jtnode, bool include_joins);
+extern Relids get_relids_for_join(PlannerInfo *root, int joinrelid);
+extern void pull_up_subquery_hint(PlannerInfo *root, Query *parse, HintState *hint_state);
+extern void preprocess_rownum(PlannerInfo *root, Query *parse);
 
 /*
  * prototypes for prepnonjointree.cpp
  */
-extern bool get_real_rte_varno_attno(
-    Query* parse, Index* varno, AttrNumber* varattno, const Index targetVarno = InvalidOid);
-extern bool is_join_inner_side(
-    const Node* fromNode, const Index targetRTEIndex, const bool isParentInnerSide, bool* isFound);
-extern Query* lazyagg_main(Query* parse);
-extern void reduce_orderby(Query* query, bool reduce);
+extern bool get_real_rte_varno_attno(Query *parse, Index *varno, AttrNumber *varattno,
+    const Index targetVarno = InvalidOid);
+extern bool is_join_inner_side(const Node *fromNode, const Index targetRTEIndex, const bool isParentInnerSide,
+    bool *isFound);
+extern Query *lazyagg_main(Query *parse);
+extern void reduce_orderby(Query *query, bool reduce);
 
-extern Node* get_real_rte_varno_attno_or_node(Query* parse, Index* varno, AttrNumber* varattno);
+extern Node *get_real_rte_varno_attno_or_node(Query *parse, Index *varno, AttrNumber *varattno);
 
 /*
  * prototypes for prepqual.c
  */
-extern Node* negate_clause(Node* node);
-extern Expr* canonicalize_qual(Expr* qual);
+extern Node *negate_clause(Node *node);
+extern Expr *canonicalize_qual(Expr *qual);
 
 /*
  * prototypes for preptlist.c
  */
-extern List* preprocess_targetlist(PlannerInfo* root, List* tlist);
-extern List* preprocess_upsert_targetlist(List* tlist, int result_relation, List* range_table);
-extern PlanRowMark* get_plan_rowmark(List* rowmarks, Index rtindex);
+extern List *preprocess_targetlist(PlannerInfo *root, List *tlist);
+extern List *preprocess_upsert_targetlist(List *tlist, int result_relation, List *range_table);
+extern PlanRowMark *get_plan_rowmark(List *rowmarks, Index rtindex);
 
 /*
  * prototypes for prepunion.c
  */
-extern Plan* plan_set_operations(PlannerInfo* root, double tuple_fraction, List** sortClauses);
+extern Plan *plan_set_operations(PlannerInfo *root, double tuple_fraction, List **sortClauses);
 
-extern void expand_inherited_tables(PlannerInfo* root);
-extern void make_inh_translation_list(
-    Relation oldrelation, Relation newrelation, Index newvarno, List** translated_vars);
-extern Bitmapset* translate_col_privs(const Bitmapset* parent_privs, List* translated_vars);
+extern void expand_inherited_tables(PlannerInfo *root);
+extern void make_inh_translation_list(Relation oldrelation, Relation newrelation, Index newvarno,
+    List **translated_vars);
+extern Bitmapset *translate_col_privs(const Bitmapset *parent_privs, List *translated_vars);
 
-extern Node* adjust_appendrel_attrs(PlannerInfo* root, Node* node, AppendRelInfo* appinfo);
+extern Node *adjust_appendrel_attrs(PlannerInfo *root, Node *node, AppendRelInfo *appinfo);
+
+inline void free_quals(Query *parse)
+{
+    pfree(parse->jointree->quals);
+    parse->jointree->quals = NULL;
+}
+
+/* judge if it is possible to optimize rownum */
+inline bool verify_rownum_optimization_possibility(Query *parse)
+{
+    if (parse == NULL) {
+        return false;
+    }
+
+    if ((parse->commandType != CMD_SELECT) && (parse->commandType != CMD_INSERT) &&
+        (parse->commandType != CMD_UPDATE) && (parse->commandType != CMD_DELETE)) {
+        return false;
+    }
+
+    if (!IsA((Node *)parse->jointree, FromExpr)) {
+        return false;
+    }
+
+    if (!contain_rownum_walker(((FromExpr *)parse->jointree)->quals, NULL)) {
+        return false;
+    }
+
+    return true;
+}
+
+inline bool contain_optimizable_rownum_opexpr(OpExpr *expr)
+{
+    if (!IsA((Node *)linitial(expr->args), Rownum))
+        return false;
+
+    if (!IsA((Node *)llast(expr->args), Const))
+        return false;
+
+    if (((Const *)llast(expr->args))->consttype != INT8OID && ((Const *)llast(expr->args))->consttype != INT4OID) {
+        return false;
+    }
+
+    return true;
+}
+
+inline void rewrite_rownum_to_limit(Query *parse, Node *valueNode, int num)
+{
+    ((Const *)valueNode)->constvalue = num;
+    if (parse->limitCount != NULL) {
+        Const *Orin = (Const *)parse->limitCount;
+        parse->limitCount = (Orin->constvalue < ((Const *)valueNode)->constvalue) ? (Node *)Orin : valueNode;
+    } else {
+        parse->limitCount = valueNode;
+    }
+}
+
 
 #endif /* PREP_H */
