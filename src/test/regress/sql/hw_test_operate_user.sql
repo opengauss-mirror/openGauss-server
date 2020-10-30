221307b8975dd23cc0dc1e3e941f76fd66ffd5de
syj <904673462@qq.com>
2020-10-30 09:41:10 +0800

    Fix bug of invalid username/password when "SET ROLE role_name PASSWORD 'passwd'" is used in loop.

--- src/test/regress/sql/hw_test_operate_user.sql
+++ src/test/regress/sql/hw_test_operate_user.sql
@@ -1,6 +1,17 @@
 create role test_myrole001 with password "ttest@123";
 create role test_myrole002 with sysadmin password "ttest@123";
 
+-- test "SET ROLE role_name PASSWORD 'passwd'" in loop
+BEGIN
+	FOR i in 1..5 LOOP
+		SET ROLE test_myrole001 PASSWORD 'ttest@123';
+		RESET ROLE;
+		SET ROLE test_myrole002 PASSWORD 'ttest@123';
+		RESET ROLE;
+	END LOOP;
+END;
+/
+
 set role test_myrole001  password "ttest@123";
 alter role test_myrole002 SET maintenance_work_mem = 100000; 
 alter role test_myrole002 rename to temp_myrole;
