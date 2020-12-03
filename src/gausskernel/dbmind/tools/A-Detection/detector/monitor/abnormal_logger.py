a47d9107126bbd8ba075f6b124242b4be8484770
l00520705 <likun75@huawei.com>
2020-12-03 16:23:34 +0800

    add A-Detection

---
+++ src/gausskernel/dbmind/tools/A-Detection/detector/monitor/abnormal_logger.py
@@ -0,0 +1,15 @@
+import os
+from configparser import ConfigParser
+
+from main import config_path
+from utils import detection_logger
+
+config = ConfigParser()
+config.read(config_path)
+log_dir_realpath = os.path.realpath(config.get('log', 'log_dir'))
+if not os.path.exists(log_dir_realpath):
+    os.makedirs(log_dir_realpath)
+
+logger = detection_logger(level='INFO',
+                          log_name='abnormal',
+                          log_path=os.path.join(log_dir_realpath, 'abnormal.log'))
---
+++ src/gausskernel/dbmind/tools/A-Detection/detector/server/app.py
@@ -0,0 +1,12 @@
+class App(object):
+    def __init__(self):
+        pass
+
+    def add_resources(self):
+        pass
+
+    def init_database(self):
+        pass
+
+    def start_service(self, *args, **kwargs):
+        pass
