b0d23d9d84eb82262dfb605308ca44470e803724
chenxiaobin <1025221611@qq.com>
2021-04-16 14:38:36 +0800

    optimize primary hang scenario when standby in catchup

--- src/gausskernel/storage/replication/walsender.cpp
+++ src/gausskernel/storage/replication/walsender.cpp
@@ -117,6 +117,11 @@ long g_logical_slot_sleep_time = 0;
 #define AmWalSenderToDummyStandby() (t_thrd.walsender_cxt.MyWalSnd->sendRole == SNDROLE_PRIMARY_DUMMYSTANDBY)
 #define AmWalSenderOnDummyStandby() (t_thrd.walsender_cxt.MyWalSnd->sendRole == SNDROLE_DUMMYSTANDBY_STANDBY)
 
+/*
+ * calculate catchup late every 1000ms
+ */
+#define CALCULATE_CATCHUP_RATE_TIME 1000
+
 /* Statistics for log control */
 static const int MICROSECONDS_PER_SECONDS = 1000000;
 static const int MILLISECONDS_PER_SECONDS = 1000;
@@ -208,6 +213,7 @@ static void WalSndRefreshPercentCountStartLsn(XLogRecPtr currentMaxLsn, XLogRecP
 static void set_xlog_location(ServerMode local_role, XLogRecPtr* sndWrite, XLogRecPtr* sndFlush, XLogRecPtr* sndReplay);
 static void ProcessArchiveFeedbackMessage(void);
 static void WalSndArchiveXlog(XLogRecPtr targetLsn, int sub_term);
+static void CalCatchupRate();
 
 char *DataDir = ".";
 
@@ -3325,13 +3331,8 @@ static int WalSndLoop(WalSndSendDataCallback send_data)
                 }
             }
         } else {
-            if (t_thrd.walsender_cxt.MyWalSnd->state == WALSNDSTATE_STREAMING &&
-                !XLByteLT(t_thrd.walsender_cxt.catchup_threshold,
-                          INT2UINT64(g_instance.attr.attr_storage.MaxSendSize) * 1024)) {
-                ereport(DEBUG1, (errmsg("standby \"%s\" has now caught up with primary",
-                                        u_sess->attr.attr_common.application_name)));
-                WalSndSetState(WALSNDSTATE_CATCHUP);
-                t_thrd.walsender_cxt.catchup_threshold = 0;
+            if (t_thrd.walsender_cxt.MyWalSnd->state == WALSNDSTATE_CATCHUP) {
+                CalCatchupRate();
             }
         }
 
@@ -3618,6 +3619,9 @@ static void InitWalSnd(void)
             walsnd->log_ctrl.pre_rate2 = 0;
             walsnd->log_ctrl.prev_RPO = -1;
             walsnd->log_ctrl.current_RPO = -1;
+            walsnd->lastCalTime = 0;
+            walsnd->lastCalWrite = InvalidXLogRecPtr;
+            walsnd->catchupRate = 0;
             walsnd->log_ctrl.just_keep_alive = false;
             SpinLockRelease(&walsnd->mutex);
             /* don't need the lock anymore */
@@ -5467,4 +5471,34 @@ static void WalSndRefreshPercentCountStartLsn(XLogRecPtr currentMaxLsn, XLogRecP
 XLogSegNo WalGetSyncCountWindow(void)
 {
     return (XLogSegNo)(uint32)u_sess->attr.attr_storage.wal_keep_segments;
+}
+
+/*
+ * Calculate catchup rate of standby to estimate how long
+ * the standby will be caught up with primary.
+ */
+static void CalCatchupRate() {
+    if (g_instance.attr.attr_storage.catchup2normal_wait_time < 0) {
+        return;
+    }
+    volatile WalSnd *walsnd = t_thrd.walsender_cxt.MyWalSnd;
+
+    SpinLockAcquire(&walsnd->mutex);
+    XLogRecPtr write = walsnd->write;
+    TimestampTz now = GetCurrentTimestamp();
+
+    if (XLByteEQ(walsnd->lastCalWrite, InvalidXLogRecPtr)) {
+        walsnd->lastCalWrite = write;
+        walsnd->lastCalTime = now;
+        SpinLockRelease(&walsnd->mutex);
+        return;
+    }
+    if (TimestampDifferenceExceeds(walsnd->lastCalTime, now, CALCULATE_CATCHUP_RATE_TIME)) {
+        double tempRate = (double)(now - walsnd->lastCalTime) /
+                          (double)XLByteDifference(write, walsnd->lastCalWrite);
+        walsnd->catchupRate = walsnd->catchupRate == 0 ? tempRate : (walsnd->catchupRate + tempRate) / 2;
+        walsnd->lastCalWrite = write;
+        walsnd->lastCalTime = now;
+    }
+    SpinLockRelease(&walsnd->mutex);
 }
