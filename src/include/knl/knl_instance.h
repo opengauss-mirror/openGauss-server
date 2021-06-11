fe108e508151a973f98a4a64891c4d9500a97331
Racardo.Cui <wwwcuiwei@gmail.com>
2021-06-11 10:58:08 +0800

    RAS:基于4P pro的UCE故障感知

--- src/include/knl/knl_instance.h
+++ src/include/knl/knl_instance.h
@@ -764,6 +764,11 @@ typedef struct knl_g_hypo_context {
     List* hypo_index_list;
 } knl_g_hypo_context;
 
+typedef struct knl_sigbus_context {
+    void* sigbus_addr;
+    int sigbus_code;
+} knl_sigbus_context;
+
 typedef struct knl_instance_context {
     knl_virtual_role role;
     volatile int status;
@@ -861,6 +866,7 @@ typedef struct knl_instance_context {
     knl_g_archive_standby_context archive_standby_cxt;
     struct HTAB* ngroup_hash_table;
     knl_g_hypo_context hypo_cxt;
+    knl_sigbus_context sigbus_cxt;
 } knl_instance_context;
 
 extern long random();
