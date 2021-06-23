bd515dfada59bc5e8066b758bb9d564d1be7e405
TotaJ <totaj@qq.com>
2021-06-23 18:30:24 +0800

    Fix rename schema bug.

--- src/test/regress/sql/create_schema.sql
+++ src/test/regress/sql/create_schema.sql
@@ -1,11 +1,15 @@
 create role samedb_schema_cn_role_02_001 password 'Ttest_234';
 
 create schema authorization samedb_schema_cn_role_02_001
-create table samedb_schema_cn_role_02_001.cn_table_00 (cn_a int, cn_b text , cn_c date ,cn_d interval)
+create table samedb_schema_cn_role_02_001.cn_table_00 (cn_a int, cn_b text , cn_c date ,cn_d interval, cn_e serial)
 create view cn_view_00 as
 select cn_b,cn_d, cn_c from cn_table_00;
 
+\d+ samedb_schema_cn_role_02_001.cn_table_00
+
 alter schema samedb_schema_cn_role_02_001 rename to  samedb_schema_cn_role_02_001_bak;
+
+\d+ samedb_schema_cn_role_02_001_bak.cn_table_00
 
 alter view samedb_schema_cn_role_02_001_bak.cn_view_00 rename to cn_view_00_bak;
 
