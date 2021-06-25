65a26243851b4b169461ef3a75152e01313d5da5
ashnah <ashnah@sina.com>
2021-06-25 17:31:38 +0800

    use holdinterrupt instead of block signal

--- src/gausskernel/cbb/instruments/statement/instr_handle_mgr.cpp
+++ src/gausskernel/cbb/instruments/statement/instr_handle_mgr.cpp
@@ -80,7 +80,7 @@ void statement_init_metric_context()
         statement_commit_metirc_context();
     }
 
-	gs_signal_setmask(&t_thrd.libpq_cxt.BlockSig, NULL);
+	HOLD_INTERRUPTS();
     (void)syscalllockAcquire(&u_sess->statement_cxt.list_protect);
 	PG_TRY();
 	{
@@ -112,7 +112,7 @@ void statement_init_metric_context()
     PG_END_TRY();	
     (void)syscalllockRelease(&u_sess->statement_cxt.list_protect);
 
-	gs_signal_setmask(&t_thrd.libpq_cxt.UnBlockSig, NULL);
+	RESUME_INTERRUPTS()();
 
     ereport(DEBUG1, (errmodule(MOD_INSTR), errmsg("[Statement] init - free list length: %d, suspend list length: %d",
         u_sess->statement_cxt.free_count, u_sess->statement_cxt.suspend_count)));
