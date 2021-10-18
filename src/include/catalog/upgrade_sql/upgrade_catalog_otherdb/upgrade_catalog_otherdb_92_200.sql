fc8fd72914298057f4c57a6926d261220f520438
Yuwei Peng <ywpeng@whu.edu.cn>
2021-10-19 00:05:22 +0800

    更正了升级脚本

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade_catalog_otherdb_92_200.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade_catalog_otherdb_92_200.sql
@@ -2393,21 +2393,3 @@ out catalog_version_no pg_catalog.int4,
 out system_identifier pg_catalog.int8,
 out pg_control_last_modified pg_catalog.timestamptz)
 RETURNS SETOF record LANGUAGE INTERNAL VOLATILE STRICT as 'pg_control_system';
-
-DROP FUNCTION IF EXISTS pg_catalog.pg_buffercache_pages() CASCADE;
-
-SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 4130;
-CREATE OR REPLACE FUNCTION pg_catalog.pg_buffercache_pages
-(out bufferid pg_catalog.int4,
-out relfilenode pg_catalog.oid,
-out bucketid pg_catalog.int2,
-out storage_type pg_catalog.int2,
-out reltablespace pg_catalog.oid,
-out reldatabase pg_catalog.oid,
-out relforknumber pg_catalog.int2,
-out relblocknumber pg_catalog.int8,
-out isdirty pg_catalog.bool,
-out isvalid pg_catalog.bool,
-out usage_count pg_catalog.int2,
-out pinning_backends pg_catalog.int4)
-RETURNS SETOF record LANGUAGE INTERNAL STABLE STRICT as 'pg_buffercache_pages';
