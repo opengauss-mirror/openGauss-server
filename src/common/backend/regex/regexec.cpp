955c8b4383eaff2105025d12715cb8aa4678a4c2
邓旭玥 <dengxuyue@xfusion.com>
2022-08-04 12:30:22 +0000

    update src/common/backend/regex/regexec.cpp. return not matched if start point falls beyond the string

--- src/common/backend/regex/regexec.cpp
+++ src/common/backend/regex/regexec.cpp
@@ -181,7 +181,8 @@ int pg_regexec(regex_t* re, const chr* string, size_t len, size_t search_start, 
         return REG_INVARG;
     if (re->re_csize != sizeof(chr))
         return REG_MIXED;
-
+    if (search_start > len)
+        return REG_NOMATCH;
     /* Initialize locale-dependent support */
     pg_set_regex_collation(re->re_collation);
 
