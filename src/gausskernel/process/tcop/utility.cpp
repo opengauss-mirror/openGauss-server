aa5ffec6a39c8bb1f9bbf1c661ed2988412e5cf8
yuhuanhuan <1500773557@qq.com>
2022-10-10 10:21:17 +0800

    【fix】解决触发器中设置自定义变量报错的问题

--- src/gausskernel/process/tcop/utility.cpp
+++ src/gausskernel/process/tcop/utility.cpp
@@ -8624,7 +8624,7 @@ const char* CreateCommandTag(Node* parse_tree)
 
         case T_VariableMultiSetStmt:
             {
-                tag = "Set";
+                tag = "SET";
             } break;
 
         case T_VariableShowStmt:
