4ea8411f66905d425d6ff8be80373e1e1b83ca86
ganyang <ganyang2@huawei.com>
2022-11-25 17:51:40 +0800

    fix fulljoin rewrite for expression alias

--- src/gausskernel/optimizer/prep/prepjointree.cpp
+++ src/gausskernel/optimizer/prep/prepjointree.cpp
@@ -3555,6 +3555,7 @@ static Node* reduce_inequality_fulljoins_jointree_recurse(PlannerInfo* root, Nod
                     } break;
                     default:
                         oldvar = (Node*)copyObject(node);
+                        oldvar = eval_const_expressions(root, oldvar);
                         break;
                 }
 
