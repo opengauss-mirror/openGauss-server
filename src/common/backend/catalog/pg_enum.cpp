9d2a9e2fe91712bc8322a93d81d68e607eba8315
catik <1414334649@qq.com>
2022-12-08 20:48:29 +0800

    enum类型支持空字符串

--- src/common/backend/catalog/pg_enum.cpp
+++ src/common/backend/catalog/pg_enum.cpp
@@ -33,7 +33,7 @@ static int sort_order_cmp(const void* p1, const void* p2);
 
 #define checkEnumLableValue(val)                                                             \
     do {                                                                                     \
-        if (NAMEDATALEN - 1 < strlen(val) || 0 == strlen(val)) {                             \
+        if (NAMEDATALEN - 1 < strlen(val) || (0 == strlen(val) && !u_sess->attr.attr_sql.dolphin)) { \
             ereport(ERROR,                                                                   \
                 (errcode(ERRCODE_INVALID_NAME),                                              \
                     errmsg("invalid enum label \"%s\"", val),                                \
