7a4ed428c2a7370745d0801fcf2218d4a86f6560
chenbd <chenbd@vastdata.com.cn>
2022-12-27 04:13:50 -0500

    修复psql 和B模式的delimiter 冲突的问题

--- src/test/regress/sql/mysql_delimiter.sql
+++ src/test/regress/sql/mysql_delimiter.sql
@@ -58,5 +58,73 @@ delimiter "sdsd sd"
 select 1"sdsd sd"
 delimiter ;
 
+-- test delimiter use in create procedure situation
+-- report gram error in server ,not subprogram end error, success in plugin 
+create table test_table (dot_no int);
+insert into test_table values(1);
+insert into test_table values(NULL);
+
+delimiter //
+
+create procedure test91()
+begin
+  declare rec_curs_value int;
+  declare curs_dot cursor for select dot_no from test_table;
+    open curs_dot;
+    fetch curs_dot into rec_curs_value;
+    while rec_curs_value is not null do
+      fetch curs_dot into rec_curs_value;
+    end while;
+    close curs_dot;
+end;
+//
+delimiter ;
+
+
+delimiter //
+
+create procedure test92()
+begin
+  declare rec_curs_value int;
+  declare curs_dot cursor for select dot_no from test_table;
+    open curs_dot;
+    fetch curs_dot into rec_curs_value;
+    while rec_curs_value is null do
+      fetch curs_dot into rec_curs_value;
+    end while;
+    close curs_dot;
+end;
+//
+
+delimiter ;
+
+
+-- test deterministic error
+
+create function fun2(age1 int)return int DETERMINISTIC 
+NOT SHIPPABLE NOT FENCED EXTERNAL SECURITY INVOKER  
+AS 
+declare
+a1 int;
+begin
+return a1;
+end;
+/
+
+select fun2(1);
+
+-- test create procedure select error 
+create table t1 (a int);
+
+insert into t1 values (1),(2);
+
+-- server should has gram error,plugin pass
+create procedure pro_test() select a from t1;
+
+-- server should has gram error, plugin pass;
+create procedure pro_test2() select a as b from t1;
+
+
+
 \c regression
 drop database my_test;
