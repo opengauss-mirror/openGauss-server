2b6568151febf242c63a7fcfbf3a1a41d318c33d
arcoalien@qq.com <arcoalien@qq.com>
2023-03-09 16:42:28 +0800

    修复备机在readbuffer->seg_read->lock seg head buffer卡住后主机reform卡的问题

--- src/gausskernel/storage/access/redo/redo_segpage.cpp
+++ src/gausskernel/storage/access/redo/redo_segpage.cpp
@@ -32,6 +32,7 @@
 #include "commands/tablespace.h"
 #include "catalog/storage_xlog.h"
 #include "storage/smgr/fd.h"
+#include "ddes/dms/ss_dms_bufmgr.h"
 
 static XLogRecParseState *segpage_redo_parse_seg_truncate_to_block(XLogReaderState *record, uint32 *blocknum)
 {
@@ -455,6 +456,8 @@ void SegPageRedoNewPage(XLogBlockHead *blockhead, XLogBlockSegNewPage *newPageIn
 
 void MarkSegPageRedoChildPageDirty(RedoBufferInfo *bufferinfo)
 {
+    SSMarkBufferDirtyForERTO(bufferinfo);
+
     BufferDesc *bufDesc = GetBufferDescriptor(bufferinfo->buf - 1);
     if (bufferinfo->dirtyflag || XLByteLT(bufDesc->extra->lsn_on_disk, PageGetLSN(bufferinfo->pageinfo.page))) {
         if (IsSegmentPhysicalRelNode(bufferinfo->blockinfo.rnode)) {
@@ -487,6 +490,8 @@ void MarkSegPageRedoChildPageDirty(RedoBufferInfo *bufferinfo)
     } else {
         UnlockReleaseBuffer(bufferinfo->buf);
     }
+
+    SSMarkBufferDirtyForERTO(bufferinfo);
 }
 
 void SegPageRedoChildState(XLogRecParseState *childStateList)
