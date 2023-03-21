1789b27f7f31f21e8dff75c55457c6c10f267a4f
laishenghao <laishenghao1@huawei.com>
2023-03-21 19:32:42 +0800

    fixed 5b839e5 from https://gitee.com/dodders/openGauss-server/pulls/3219 解决增量物化视图存在匿名类型时，无法级联删除表格的问题

--- src/test/regress/sql/matview_single.sql
+++ src/test/regress/sql/matview_single.sql
@@ -16,3 +16,46 @@ drop Materialized view mvtest_tm;
 -- error
 create incremental MATERIALIZED VIEW mvtest_inc AS select *from test;
 REFRESH incremental MATERIALIZED VIEW mvtest_tm;
+
+-- test matview with anounymous types
+create database test_imv_db with dbcompatibility 'b';
+\c test_imv_db
+
+-- case 1: drop type first
+create table imv1_t(a set('ad','asf'), c int);
+create incremental materialized view imv1_v as select * from imv1_t;
+create incremental materialized view imv12_v as select * from imv1_t;
+drop type imv1_t_a_set cascade;
+select oid, relname from pg_class where relname like 'mlog%';
+drop table imv1_t cascade;
+select oid, relname from pg_class where relname like 'mlog%';
+
+-- case 2: drop view first
+create table imv1_t(a set('ad','asf'), c int);
+create incremental materialized view imv1_v as select * from imv1_t;
+create incremental materialized view imv12_v as select * from imv1_t;
+drop materialized view imv1_v;
+select oid, relname from pg_class where relname like 'mlog%';
+drop materialized view imv12_v;
+select oid, relname from pg_class where relname like 'mlog%';
+drop table imv1_t cascade;
+
+-- case 3: drop table directly
+create table imv1_t(a set('ad','asf'), c int);
+create incremental materialized view imv1_v as select * from imv1_t;
+create incremental materialized view imv12_v as select * from imv1_t;
+drop table imv1_t cascade;
+select oid, relname from pg_class where relname like 'mlog%';
+
+-- case 4: multi columns
+create table imv1_t(a set('ad','asf'), b set('b', 'bb'), c int, d set('b', 'bb'));
+create incremental materialized view imv1_v as select * from imv1_t;
+create incremental materialized view imv12_v as select * from imv1_t;
+create incremental materialized view imv13_v as select * from imv1_t;
+\d
+drop table imv1_t cascade;
+select oid, relname from pg_class where relname like 'mlog%';
+\d
+
+\c regression
+drop database test_imv_db;
