12bfb41e71f919d9225314e956328204db5a5c5b
shirley_zhengx <zhengxue6@huawei.com>
2023-04-08 18:04:42 +0800

    修复轻量级编译error

--- src/bin/pg_ctl/pg_ctl.cpp
+++ src/bin/pg_ctl/pg_ctl.cpp
@@ -6686,6 +6686,7 @@ int main(int argc, char** argv)
                     }
                     break;
                 }
+#ifndef ENABLE_LITE_MODE
                 case 5:{
                     check_input_for_security(optarg);
                     if (strlen(optarg) > MAX_PATH_LEN) {
@@ -6727,6 +6728,7 @@ int main(int argc, char** argv)
                     ss_nodedatainfo = xstrdup(optarg);
                     break;
                 }
+#endif
                 default:
                     /* getopt_long already issued a suitable error message */
                     do_advice();
