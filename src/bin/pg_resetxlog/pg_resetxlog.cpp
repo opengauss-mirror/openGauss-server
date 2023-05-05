fd2b87664ebba4df5640d829fd5eff58f6faf0e9
totaj <totaj@qq.com>
2023-05-05 11:11:48 +0800

    Add negative option test for pg_resetxlog.

--- src/bin/pg_resetxlog/pg_resetxlog.cpp
+++ src/bin/pg_resetxlog/pg_resetxlog.cpp
@@ -97,6 +97,15 @@ typedef struct DssOptions
 /* DSS connect parameters */
 static DssOptions dss;
 
+static inline bool is_negative_num(char *str)
+{
+    char *s = str;
+    while (*s != '\0' && isspace(*s)) {
+        s++;
+    }
+    return *s == '-' ? true : false;
+}
+
 int main(int argc, char* argv[])
 {
     int c;
@@ -161,6 +170,10 @@ int main(int argc, char* argv[])
                     fprintf(stderr, _("Try \"%s --help\" for more information.\n"), progname);
                     exit(1);
                 }
+                if (is_negative_num(optarg)) {
+                    fprintf(stderr, _("%s: transaction ID epoch (-e) can't be negative.\n"), progname);
+                    exit(1);
+                }
                 break;
 
             case 'x':
@@ -170,8 +183,14 @@ int main(int argc, char* argv[])
                     fprintf(stderr, _("Try \"%s --help\" for more information.\n"), progname);
                     exit(1);
                 }
-                if (set_xid == 0) {
-                    fprintf(stderr, _("%s: transaction ID (-x) must not be 0\n"), progname);
+                if (is_negative_num(optarg)) {
+                    fprintf(stderr, _("%s: transaction ID (-x) can't be negative.\n"), progname);
+                    exit(1);
+                }
+
+                if (!TransactionIdIsNormal(set_xid)) {
+                    fprintf(stderr, _("%s: transaction ID (-x) must be greater than or equal to %lu.\n"),
+                        progname, FirstNormalTransactionId);
                     exit(1);
                 }
                 break;
@@ -181,6 +200,10 @@ int main(int argc, char* argv[])
                 if (endptr == optarg || *endptr != '\0' || tmpValue > PG_UINT32_MAX) {
                     fprintf(stderr, _("%s: invalid argument for option -o\n"), progname);
                     fprintf(stderr, _("Try \"%s --help\" for more information.\n"), progname);
+                    exit(1);
+                }
+                if (is_negative_num(optarg)) {
+                    fprintf(stderr, _("%s: OID (-o) can't be negative.\n"), progname);
                     exit(1);
                 }
                 set_oid = (Oid)tmpValue;
@@ -195,6 +218,10 @@ int main(int argc, char* argv[])
                 if (endptr == optarg || *endptr != '\0') {
                     fprintf(stderr, _("%s: invalid argument for option -m\n"), progname);
                     fprintf(stderr, _("Try \"%s --help\" for more information.\n"), progname);
+                    exit(1);
+                }
+                if (is_negative_num(optarg)) {
+                    fprintf(stderr, _("%s: multitransaction ID (-m) can't be negative.\n"), progname);
                     exit(1);
                 }
                 if (set_mxid == 0) {
