3774c74df328d0c9b0ca6c090807d5c605b76410
gentle_hu <gentle_hu@qq.com>
2023-06-20 17:56:29 +0800

    fix bug about recyclebin and truncate partation table

--- src/common/backend/utils/cache/relcache.cpp
+++ src/common/backend/utils/cache/relcache.cpp
@@ -4922,7 +4922,7 @@ RelFileNodeBackend CreateNewRelfilenodePart(Relation parent, Partition part)
         part->newcbi = true;
     }
 
-    partition_create_new_storage(parent, part, newrnode);
+    partition_create_new_storage(parent, part, newrnode, true);
 
     return newrnode;
 }
