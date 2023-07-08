a52d7119664738432f9e912440bcd8f4d148da7d
totaj <totaj@qq.com>
2023-07-08 16:34:53 +0800

    fixed 9985b86 from https://gitee.com/totaj/openGauss-server/pulls/3682 Fix upgrade/rollback from 3.1.0

--- src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_92_830.sql
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_92_830.sql
@@ -1,1 +1,8 @@
-drop extension if exists dolphin;
+DO $$
+BEGIN
+    -- 92780 is openGauss 3.1.0, which has dolphin, so should not drop it.
+    if working_version_num() < 92780 then
+        drop extension if exists dolphin;
+    end if;
+END
+$$;
