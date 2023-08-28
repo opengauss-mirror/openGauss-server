4194a92fcd7bdfc930198f1e1c5e620c40743213
luozihao <1165977584@qq.com>
2023-08-28 20:00:36 +0800

    fixed a6720f2 from https://gitee.com/luo_zihao5524/openGauss-server/pulls/4024 修复interval类型在存储过程复制流程中赋值报错的bug

--- src/test/regress/sql/interval.sql
+++ src/test/regress/sql/interval.sql
@@ -368,3 +368,42 @@ select '15'::numeric::INTERVAL YEAR;
 select '15'::text::INTERVAL YEAR;
 select '15'::varchar::INTERVAL YEAR;
 select '15'::bpchar(2)::INTERVAL YEAR;
+
+-- test abourt interval typmod in procedure
+drop table if exists all_datatype_tbl;
+create table all_datatype_tbl(
+        c_id integer,
+        c_boolean boolean,
+        c_integer integer, c_bigint bigint,
+        c_real real,
+        c_decimal decimal(38), c_number number(38),
+        c_char char(50) default null, c_varchar varchar(50), c_clob clob,
+    c_blob blob,
+         c_timestamp timestamp, c_interval interval day to second) with (segment=on) ;
+create or replace procedure pro_012()
+as
+    sqlstat varchar(500);
+        v1 interval day to second;
+begin
+    v1 := '12 12:3:4.1234';
+    sqlstat := 'insert into all_datatype_tbl(c_interval) select :p1';
+    execute immediate sqlstat using v1;
+end;
+/
+call pro_012();
+select c_interval from all_datatype_tbl;
+drop procedure pro_012;
+create or replace procedure pro_015()
+as
+    sqlstat varchar(500);
+        v1 interval day to second;
+        r1 interval day to second;
+begin
+    v1 := '12 12:3:4.1234';
+    sqlstat := 'select :p1';
+    execute immediate sqlstat into r1 using v1;
+    raise info 'result:%',v1;
+end;
+/
+call pro_015();
+drop procedure pro_015;
