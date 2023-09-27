563971b4c27e9574e395f309f0b04fc0e5ab09f6
movead <lchch1990@sina.cn>
2023-09-27 09:54:07 +0800

    for parallel redo, change txn check lsn method from finish lsn to tying lsn

--- src/gausskernel/storage/access/transam/parallel_recovery/page_redo.cpp
+++ src/gausskernel/storage/access/transam/parallel_recovery/page_redo.cpp
@@ -411,6 +411,7 @@ void ApplyProcHead(RedoItem *head)
     while (head != NULL) {
         RedoItem *cur = head;
         g_redoWorker->current_item = &cur->record;
+        pg_atomic_write_u64((volatile uint64*)&g_redoWorker->curReplayingReadRecPtr, cur->record.ReadRecPtr);
         head = head->nextByWorker[g_redoWorker->id + 1];
         ApplyAndFreeRedoItem(cur);
     }
@@ -1020,6 +1021,12 @@ XLogRecPtr GetCompletedRecPtr(PageRedoWorker *worker)
 {
     pg_read_barrier();
     return pg_atomic_read_u64(&worker->lastReplayedEndRecPtr);
+}
+
+XLogRecPtr GetReplyingRecPtr(PageRedoWorker *worker)
+{
+    pg_read_barrier();
+    return pg_atomic_read_u64(&worker->curReplayingReadRecPtr);
 }
 
 /* automic write for lastReplayedReadRecPtr and lastReplayedEndRecPtr */
