3ca0a882e085dd0e536803901904a6df1356cb65
chenxiaobin19 <1025221611@qq.com>
2023-10-07 15:46:55 +0800

    dolphin300

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_914.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_914.sql
@@ -596,3 +596,16 @@ GRANT SELECT ON TABLE DBE_PERF.statement_history TO PUBLIC;
 END IF;
 END IF;
 END $DO$;
+
+do $$
+DECLARE
+ans boolean;
+BEGIN
+    for ans in select case when count(*)=1 then true else false end as ans  from (select extname from pg_extension where extname='dolphin')
+    LOOP
+        if ans = true then
+            ALTER EXTENSION dolphin UPDATE TO '3.0';
+        end if;
+        exit;
+    END LOOP;
+END$$;
