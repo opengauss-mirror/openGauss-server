7cd9e895b12891279b66a151f412a1b6e0d27677
congzhou2603 <zhoucong42@huawei.com>
2023-12-07 20:03:25 +0800

    【bugfix】解决备机同时配置极致RTO和延迟回放时，cannot wait on a latch owned by another process

--- src/gausskernel/storage/access/transam/xlog.cpp
+++ src/gausskernel/storage/access/transam/xlog.cpp
@@ -414,7 +414,8 @@ static void ReserveXLogInsertLocation(uint32 size, XLogRecPtr *StartPos, XLogRec
 static bool ReserveXLogSwitch(XLogRecPtr *StartPos, XLogRecPtr *EndPos, XLogRecPtr *PrevPtr);
 static void StartSuspendWalInsert(int32 *const lastlrc_ptr);
 static void StopSuspendWalInsert(int32 lastlrc);
-
+void ResetRecoveryDelayLatch();
+void WaitRecoveryDelayLatch(long waitTime);
 template <bool isGroupInsert>
 static char *GetXLogBuffer(XLogRecPtr ptr, PGPROC *proc = NULL);
 static XLogRecPtr XLogBytePosToRecPtr(uint64 bytepos);
@@ -6714,6 +6715,11 @@ void XLOGShmemInit(void)
     InitSharedLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
     InitSharedLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->dataRecoveryLatch);
 
+    /* recovery_min_apply_delay is SIGHUP level, so init recoveryWakeupDelayLatch if extremeRto mode*/
+    if (IsExtremeRedo()) {
+        InitSharedLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch);
+    }
+
     /*
      * If we are not in bootstrap mode, pg_control should already exist. Read
      * and validate it immediately (see comments in ReadControlFile() for the
@@ -8111,7 +8117,7 @@ static bool RecoveryApplyDelay(const XLogReaderState *record)
     }
 
     while (true) {
-        ResetLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
+        ResetRecoveryDelayLatch();
 
         /* might change the trigger file's location */
         RedoInterruptCallBack();
@@ -8133,7 +8139,7 @@ static bool RecoveryApplyDelay(const XLogReaderState *record)
 
         /* delay 1s every time */
         waitTime = (secs >= 1) ? 1000 : (microsecs / 1000);
-        (void)WaitLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch, WL_LATCH_SET | WL_TIMEOUT, waitTime);
+        WaitRecoveryDelayLatch(waitTime);
     }
     return true;
 }
@@ -9281,6 +9287,9 @@ void StartupXLOG(void)
     if (t_thrd.xlog_cxt.StandbyModeRequested) {
         OwnLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
         OwnLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->dataRecoveryLatch);
+        if (IsExtremeRedo()) {
+            OwnLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch);
+        }
     }
 
     /* Set up XLOG reader facility */
@@ -10293,6 +10302,9 @@ void StartupXLOG(void)
             DisownLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
         }
         DisownLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->dataRecoveryLatch);
+        if (IsExtremeRedo()) {
+            DisownLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch);
+        }
     }
 
     /*
@@ -18066,6 +18078,9 @@ int CheckSwitchoverSignal(void)
 void WakeupRecovery(void)
 {
     SetLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
+    if (IsExtremeRedo() && u_sess->attr.attr_storage.recovery_min_apply_delay > 0) {
+        SetLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch);
+    }
 }
 
 void WakeupDataRecovery(void)
@@ -18694,6 +18709,28 @@ void GetRecoveryLatch()
 void ReLeaseRecoveryLatch()
 {
     DisownLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
+}
+
+/**
+ * Reset latch before startup process delay recovery.
+ */
+void ResetRecoveryDelayLatch() {
+    if (IsExtremeRedo()) {
+        ResetLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch);
+    } else {
+        ResetLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch);
+    }
+}
+
+/**
+ * Let startup process wait for a while until waitTime ms or other threads set latch.
+ */
+void WaitRecoveryDelayLatch(long waitTime) {
+    if (IsExtremeRedo()) {
+        (void)WaitLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupDelayLatch, WL_LATCH_SET | WL_TIMEOUT, waitTime);
+    } else {
+        (void)WaitLatch(&t_thrd.shemem_ptr_cxt.XLogCtl->recoveryWakeupLatch, WL_LATCH_SET | WL_TIMEOUT, waitTime);
+    }
 }
 
 XLogRecPtr mpfl_get_max_lsn_from_buf(char *buf)
