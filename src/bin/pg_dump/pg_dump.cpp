956f2e043296a33936f46c5d71575e2f2bd298a2
wuyuechuan <wuych9@mail2.sysu.edu.cn>
2023-12-13 16:03:04 +0800

    gs_dump导出rename过的schema下的表的auto_increment失败

--- src/bin/pg_dump/pg_dump.cpp
+++ src/bin/pg_dump/pg_dump.cpp
@@ -23576,13 +23576,11 @@ static void dumpTableAutoIncrement(Archive* fout, PQExpBuffer sqlbuf, TableInfo*
     /* Make sure we are in proper schema */
     selectSourceSchema(fout, tbinfo->dobj.nmspace->dobj.name);
     /* Obtain the sequence name of the auto_increment column from the pg_attrdef. */
-    appendPQExpBuffer(query,
-        "SELECT pg_catalog.split_part(pg_catalog.split_part(adbin, ':seqNameSpace ', 2), ' ', 1) as seqnamespace, "
-        "pg_catalog.replace(pg_catalog.replace(pg_catalog.replace(pg_catalog.replace(pg_catalog.replace(pg_catalog.replace"
-        "(pg_catalog.split_part(pg_catalog.split_part(adbin, ':seqName ', 2), ' :seqNameSpace', 1), '\\ ', ' '), "
-        "'\\{', '{'), '\\}', '}'), '\\(', '('), '\\)', ')'), '\\\\', '\\') as seqname "
-        "from pg_catalog.pg_attrdef where adrelid = %u and adnum = %d",
-        tbinfo->dobj.catId.oid, tbinfo->autoinc_attnum);
+    appendPQExpBuffer(query, "select nspname, relname from "
+                             "pg_class c left join pg_namespace n on c.relnamespace = n.oid "
+                             "left join  pg_depend d on c.oid = d.objid "
+                             "where classid = %u and deptype = '%c' and refobjid = %u and refobjsubid = %u "
+                      ,RelationRelationId, DEPENDENCY_AUTO, tbinfo->dobj.catId.oid, tbinfo->autoinc_attnum);
 
     res = ExecuteSqlQuery(fout, query->data, PGRES_TUPLES_OK);
     if (PQntuples(res) != 1) {
