756c4f7b5ddc18cd00ba2c97fbcb730029f0072a
suncan <1006949218@qq.com>
2024-02-02 17:35:59 +0800

    解决多表更部分行为与mysql存在差异问题

--- src/test/regress/sql/multi_update.sql
+++ src/test/regress/sql/multi_update.sql
@@ -12,6 +12,40 @@ insert into t0 values(25, -8, -88, -8),(-88, -77, 25, -8);
 update t0 a, t0 b set b.c1=10, a.c2=200, b.c3=20, a.c4=100;
 select * from t0;
 drop table t0;
+drop table if exists tt0;
+create table tt0(a int, b int);
+insert into tt0 values(1,2),(2,3);
+begin;
+update tt0 a0, tt0 a1 set a0.a=5;
+select * from tt0;
+rollback;
+begin;
+update tt0 a, tt0 b set a.a=10;
+select * from tt0;
+rollback;
+drop table tt0;
+create type newtype as(a int, b int);
+create table test(a newtype,b int);
+insert into test values(ROW(1,2),3);
+update test set test.a=ROW(10,20);
+select * from test;
+update test t set t.a=ROW(11,21);
+select * from test;
+--Ambiguous scene
+--update field a of column a rather than column a of table a
+update test a set a.a=12;
+--update field b of column a rather than column b of table a
+update test a set a.b=22;
+select * from test;
+--fail
+update test a set a.a=ROW(13,23);
+update test a set a.c=10;
+update test b set b.c=10;
+--must compatible with previous features, though not perfect
+update test a set a.a.a=12;
+select * from test;
+drop table test;
+drop type newtype;
 -- three relation
 drop table if exists t_t_mutil_t1;
 drop table if exists t_t_mutil_t2;
