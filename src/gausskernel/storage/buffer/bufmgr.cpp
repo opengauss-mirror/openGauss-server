16de9bd7994e35bf1b08dffc26e5f6a71e53489d
yunlongwang <yunlong.wang@enmotech.com>
2024-03-27 08:24:58 +0000

    回放 & debug 模式下，当从候选队列拿到的 buffer 又被写脏时，降低日志级别

--- src/gausskernel/storage/buffer/bufmgr.cpp
+++ src/gausskernel/storage/buffer/bufmgr.cpp
@@ -2623,9 +2623,6 @@ void PageCheckIfCanEliminate(BufferDesc *buf, uint32 *oldFlags, bool *needGetLoc
     if ((*oldFlags & BM_TAG_VALID) && !XLByteEQ(buf->extra->lsn_on_disk, PageGetLSN(tmpBlock)) &&
         !(*oldFlags & BM_DIRTY) && RecoveryInProgress()) {
         int mode = DEBUG1;
-#ifdef USE_ASSERT_CHECKING
-        mode = PANIC;
-#endif
         const uint32 shiftSize = 32;
         ereport(mode, (errmodule(MOD_INCRE_BG),
                 errmsg("check lsn is not matched on disk:%X/%X on page %X/%X, relnode info:%u/%u/%u %u %u",
