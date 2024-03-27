0ca808817597b5ae56b8ed18f9aa9e40591f6040
chenxiaobin19 <1025221611@qq.com>
2024-03-27 11:56:08 +0800

    修复3.1.0升级到6.0.0失败的问题

--- src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_92_806.sql
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_92_806.sql
@@ -1,3 +1,8 @@
+DO $upgrade$
+BEGIN
+IF working_version_num() < 92780 then
 DROP FUNCTION IF EXISTS pg_catalog.sha(text) CASCADE;
 DROP FUNCTION IF EXISTS pg_catalog.sha1(text) CASCADE;
 DROP FUNCTION IF EXISTS pg_catalog.sha2(text, bigint) CASCADE;
+END IF;
+END $upgrade$;
