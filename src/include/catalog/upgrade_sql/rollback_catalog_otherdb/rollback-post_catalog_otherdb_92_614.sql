c7649064a872cd34289c266226fe7434e8e96332
movead <lchch1990@sina.cn>
2024-04-02 10:57:56 +0800

    fix xid_age function show negative result if there be big gap

---
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_92_614.sql
@@ -0,0 +1,4 @@
+DROP FUNCTION IF EXISTS pg_catalog.age(xid);
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 1181;
+CREATE OR REPLACE FUNCTION pg_catalog.age(xid)
+RETURNS int LANGUAGE internal STRICT STABLE NOT FENCED as 'xid_age';
