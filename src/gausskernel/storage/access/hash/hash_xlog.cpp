6f89c74b6d25c7268f63ae82feacecb3dc873829
song.wu <song.wu@enmotech.com>
2024-05-06 17:22:36 +0800

    fix bug #I96EY4 5.0.0 上同步修复 hash index 日志回放在 hash_xlog_split_allocate_page 中 core 的bugfix

--- src/gausskernel/storage/access/hash/hash_xlog.cpp
+++ src/gausskernel/storage/access/hash/hash_xlog.cpp
@@ -232,10 +232,8 @@ static void hash_xlog_split_allocate_page(XLogReaderState *record)
     }
 
     /* replay the record for new bucket */
-    XLogInitBufferForRedo(record, 1, &newbuf);
+    XLogReadBufferForRedoExtended(record, 1, RBM_ZERO_AND_CLEANUP_LOCK, true, &newbuf);
     HashRedoSplitAllocatePageOperatorNbukPage(&newbuf, XLogRecGetData(record));
-    if (!IsBufferCleanupOK(newbuf.buf))
-        elog(PANIC, "hash_xlog_split_allocate_page: failed to acquire cleanup lock");
     MarkBufferDirty(newbuf.buf);
 
     /*
