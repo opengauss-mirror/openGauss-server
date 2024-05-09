ca6ea29d6510e191f0a6e96dba03641ed375e423
梅程 <517719039@qq.com>
2024-05-09 16:01:34 +0800

    [5..0.0]同步v3.0.5 gs_catalog_attribute_records

---
+++ src/include/catalog/upgrade_sql/rollback_catalog_maindb/rollback_catalog_maindb_92_860.sql
@@ -0,0 +1,1 @@
+DROP FUNCTION IF EXISTS pg_catalog.gs_catalog_attribute_records(oid);
---
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade_catalog_maindb_92_860.sql
@@ -0,0 +1,29 @@
+DROP FUNCTION IF EXISTS pg_catalog.gs_catalog_attribute_records(oid);
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 8010;
+CREATE OR REPLACE FUNCTION pg_catalog.gs_catalog_attribute_records(
+        IN relid oid,
+        OUT attrelid oid,
+        OUT attname name,
+        OUT atttypid oid,
+        OUT attstattarget integer,
+        OUT attlen smallint,
+        OUT attnum smallint,
+        OUT attndims integer,
+        OUT attcacheoff integer,
+        OUT atttypmod integer,
+        OUT attbyval boolean,
+        OUT attstorage "char",
+        OUT attalign "char",
+        OUT attnotnull boolean,
+        OUT atthasdef boolean,
+        OUT attisdropped boolean,
+        OUT attislocal boolean,
+        OUT attcmprmode tinyint,
+        OUT attinhcount integer,
+        OUT attcollation oid,
+        OUT attacl aclitem [],
+        OUT attoptions text [],
+        OUT attfdwoptions text [],
+        OUT attinitdefval bytea,
+        OUT attkvtype tinyint
+    ) RETURNS SETOF RECORD STRICT STABLE ROWS 1000 LANGUAGE INTERNAL AS 'gs_catalog_attribute_records';
