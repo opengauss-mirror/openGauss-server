081ef521bcbb8ae4f7ae2ee6e27363c161636e5a
chenxiaobin19 <1025221611@qq.com>
2024-08-05 11:32:22 +0800

    修复dolphin因DBE_PERF.statement重建而升级失败的问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_945.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_945.sql
@@ -1,3 +1,16 @@
+do $$
+DECLARE
+ans boolean;
+BEGIN
+    for ans in select case when count(*)=1 then true else false end as ans  from (select extname from pg_extension where extname='dolphin')
+    LOOP
+        if ans = true then
+            ALTER EXTENSION dolphin UPDATE TO '4.0.1';
+        end if;
+    exit;
+    END LOOP;
+END$$;
+
 DO $DO$
 DECLARE
 ans boolean;
@@ -701,3 +714,16 @@ CREATE OR REPLACE FUNCTION DBE_PERF.get_global_full_sql_by_timestamp
         END IF;
     END IF;
 END $DO$;
+
+do $$
+DECLARE
+ans boolean;
+BEGIN
+    for ans in select case when count(*)=1 then true else false end as ans  from (select extname from pg_extension where extname='dolphin')
+    LOOP
+        if ans = true then
+            ALTER EXTENSION dolphin UPDATE TO '4.0';
+        end if;
+    exit;
+    END LOOP;
+END$$;
