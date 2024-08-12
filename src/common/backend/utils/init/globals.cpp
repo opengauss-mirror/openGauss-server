fd2711f8bc1b88dc46ca04b4aa0b7b852c4891dd
何文健 <wenjian.he@enmotech.com>
2024-08-12 16:51:01 +0800

    fix bug #I9P3QO #5 修复gs_xlog_keepers函数在升级前后在pg_proc显示不同的bug

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -76,7 +76,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 92949;
+const uint32 GRAND_VERSION_NUM = 92950;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
