068e3a10b7024c325cc4b6c1a18f860317498494
chenxiaobin19 <1025221611@qq.com>
2024-08-12 11:18:03 +0800

    适配dolphin升级到5.0

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 92999;
+const uint32 GRAND_VERSION_NUM = 93000;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
