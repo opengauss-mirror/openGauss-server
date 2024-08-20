f7444b4f296d61da36d6be7346a15495e8823aa8
何文健 <wenjian.he@enmotech.com>
2024-08-20 19:15:05 +0800

    fix bug #I9QANR #1 #3 修复3.0..0升级至3.0.5对比直接安装的3.0.5，升级后xid_age差异问题

--- src/include/catalog/upgrade_sql/rollback_catalog_maindb/rollback-post_catalog_maindb_92_614.sql
+++ src/include/catalog/upgrade_sql/rollback_catalog_maindb/rollback-post_catalog_maindb_92_614.sql
@@ -2,3 +2,4 @@ DROP FUNCTION IF EXISTS pg_catalog.age(xid);
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 1181;
 CREATE OR REPLACE FUNCTION pg_catalog.age(xid)
 RETURNS int LANGUAGE internal STRICT STABLE NOT FENCED as 'xid_age';
+COMMENT ON FUNCTION pg_catalog.age(xid) IS 'age of a transaction ID, in transactions before current transaction';
