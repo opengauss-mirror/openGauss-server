9ae0cfd7dc11afebf904f2f9588dd91b681a17f1
梅程 <517719039@qq.com>
2024-08-29 22:10:56 +0800

    修复gs_stat_undo，gs_xlog_keepers系统函数升级问题

--- src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback_catalog_otherdb_92_935.sql
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback_catalog_otherdb_92_935.sql
@@ -9,3 +9,11 @@ DROP FUNCTION IF EXISTS pg_catalog.gs_undo_translot_dump_xid(xid, boolean, OUT z
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 4438;
 CREATE OR REPLACE FUNCTION pg_catalog.gs_undo_translot_dump_xid(xid, boolean, OUT zone_id oid, OUT slot_xid text, OUT start_undoptr text, OUT end_undoptr text, OUT lsn text, OUT gs_undo_translot oid)
 RETURNS SETOF record LANGUAGE INTERNAL as 'gs_undo_translot_dump_xid';
+
+/*------ add sys fuction gs_stat_undo ------*/
+DROP FUNCTION IF EXISTS pg_catalog.gs_stat_undo();
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 4434;
+CREATE FUNCTION pg_catalog.gs_stat_undo(OUT curr_used_zone_count int4, OUT top_used_zones text, OUT curr_used_undo_size int4,
+OUT undo_threshold int4, OUT oldest_xid_in_undo oid, OUT oldest_xmin oid, OUT total_undo_chain_len oid, OUT max_undo_chain_len oid,
+OUT create_undo_file_count int4, OUT discard_undo_file_count int4)
+RETURNS record LANGUAGE INTERNAL as 'gs_stat_undo';
