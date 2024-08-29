009a456f82580737926eb73b00869bfb5aea10a2
gentle_hu <gentle_hu@qq.com>
2024-08-29 15:16:19 +0800

    delay redo connect

--- src/gausskernel/storage/access/transam/xlog.cpp
+++ src/gausskernel/storage/access/transam/xlog.cpp
@@ -8055,6 +8055,28 @@ static bool CheckApplyDelayReady(void)
     return true;
 }
 
+static void KeepWalrecvAliveWhenRecoveryDelay()
+{
+    static TimestampTz lastTestWalrecvTime = (TimestampTz)0;
+
+    TimestampTz now = GetCurrentTimestamp();
+    if (!TimestampDifferenceExceeds(lastTestWalrecvTime, now, 1000)) {  // 1s
+        return;
+    }
+    lastTestWalrecvTime = now;
+    
+    if (WalRcvInProgress()) {
+        return;
+    }
+    
+    if (g_instance.pid_cxt.BarrierPreParsePID != 0) {
+        return;
+    }
+    
+    /* wake up walrecv by pre-parse thread */
+    g_instance.csn_barrier_cxt.pre_parse_started = false;
+}
+
 /*
  * When recovery_min_apply_delay is set, we wait long enough to make sure
  * certain record types are applied at least that interval behind the master.
@@ -8079,6 +8101,8 @@ static bool RecoveryApplyDelay(const XLogReaderState *record)
     if (!CheckApplyDelayReady()) {
         return false;
     }
+
+    KeepWalrecvAliveWhenRecoveryDelay();
 
     /*
      * Is it a COMMIT record?
@@ -8114,6 +8138,8 @@ static bool RecoveryApplyDelay(const XLogReaderState *record)
 
         /* might change the trigger file's location */
         RedoInterruptCallBack();
+
+        KeepWalrecvAliveWhenRecoveryDelay();
 
         if (CheckForFailoverTrigger() || CheckForSwitchoverTrigger() || CheckForStandbyTrigger()) {
             break;
