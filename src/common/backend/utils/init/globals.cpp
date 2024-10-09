62071b16d2c83252f5fda07e16d08ffea6ee1b9c
accept_all <lizs@vastdata.com.cn>
2024-10-09 17:58:19 +0800

    修改版本号

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 93002;
+const uint32 GRAND_VERSION_NUM = 93003;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
