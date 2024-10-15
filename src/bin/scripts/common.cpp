695744d66aa550500affdb13d5063e74b686eb39
柴毅 <chaiyi@gbase.cn>
2024-10-15 15:17:15 +0800

    #IAX8GO bin/script下的工具help命令有内存泄漏

--- src/bin/scripts/common.cpp
+++ src/bin/scripts/common.cpp
@@ -63,7 +63,9 @@ void handle_help_version_opts(int argc, char* argv[], const char* fixed_progname
 {
     if (argc > 1) {
         if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-?") == 0) {
-            hlp(get_progname(argv[0]));
+            const char* progname_tmp = get_progname(argv[0]);
+            hlp(progname_tmp);
+            free(const_cast<char*>(progname_tmp));
             exit(0);
         }
         if (strcmp(argv[1], "--version") == 0 || strcmp(argv[1], "-V") == 0) {
