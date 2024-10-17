8145aeacbcc153ed6b01a1555255b23b0967bead
zhang_xubo <2578876417@qq.com>
2024-10-17 19:24:18 +0800

    upgrade version to 3.0.6

--- src/bin/pg_config/pg_config.cpp
+++ src/bin/pg_config/pg_config.cpp
@@ -295,7 +295,7 @@ static void show_version(int alls)
 #ifdef ENABLE_MULTIPLE_NODES
     printf("PostgreSQL 9.2.4\n");
 #else
-    printf("openGauss 3.0.5\n");
+    printf("openGauss 3.0.6\n");
 #endif
 }
 
