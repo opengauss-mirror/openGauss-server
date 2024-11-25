fff356750d67e3cca57c979ffe8ee2b083962621
JulianZhang <zhangzhongliangit@chinamobile.com>
2024-11-25 10:28:31 +0800

    fix bin_to_num meta data

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade_catalog_maindb_93_009.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade_catalog_maindb_93_009.sql
@@ -1,6 +1,10 @@
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 971;
 CREATE OR REPLACE FUNCTION pg_catalog.bin_to_num(VARIADIC bins numeric[]) 
-RETURNS numeric LANGUAGE INTERNAL VOLATILE STRICT as 'bin_to_num';
+RETURNS numeric LANGUAGE INTERNAL IMMUTABLE STRICT as 'bin_to_num';
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 974;
 CREATE OR REPLACE FUNCTION pg_catalog.bin_to_num() 
-RETURNS numeric LANGUAGE INTERNAL VOLATILE as 'bin_to_num_noparam';
+RETURNS numeric LANGUAGE INTERNAL IMMUTABLE as 'bin_to_num_noparam';
+
+comment on function pg_catalog.bin_to_num(VARIADIC bins numeric[]) is 'convert bits to number';
+comment on function pg_catalog.bin_to_num() is 'convert bits to number';
+
