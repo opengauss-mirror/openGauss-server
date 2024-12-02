8244e22c7d3cb08d14a760e2fc70bd5a4fb10baf
yelingzhi <yelingzhi@shentongdata.com>
2024-12-02 06:27:34 +0000

    修复由6.0.0升级至7.0.0-RC1后json_textcontains参数类型与7.0.0-RC1直装不同的问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_018.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_018.sql
@@ -2,6 +2,6 @@ DROP FUNCTION IF EXISTS pg_catalog.json_exists(text, text, int2) CASCADE;
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 8810;
 CREATE OR REPLACE FUNCTION pg_catalog.json_exists(text, text, int2) RETURNS bool LANGUAGE INTERNAL IMMUTABLE as 'json_path_exists';
 
-DROP FUNCTION IF EXISTS pg_catalog.json_textcontains(text, text, character varying) CASCADE;
+DROP FUNCTION IF EXISTS pg_catalog.json_textcontains(text, text, cstring) CASCADE;
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 8811;
-CREATE OR REPLACE FUNCTION pg_catalog.json_textcontains(text, text, character varying) RETURNS bool LANGUAGE INTERNAL IMMUTABLE as 'json_textcontains';
+CREATE OR REPLACE FUNCTION pg_catalog.json_textcontains(text, text, cstring) RETURNS bool LANGUAGE INTERNAL IMMUTABLE as 'json_textcontains';
