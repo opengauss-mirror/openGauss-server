57391e92d46764c8cf12ed8827ab9f8458b721ba
zhangyukai <zhangyukai2@h-partners.com>
2024-12-12 16:21:02 +0800

    修复开启实时构建特性下，内置函数query_node_reform_info部分字段无法获取有效值

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 92970;
+const uint32 GRAND_VERSION_NUM = 92971;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
