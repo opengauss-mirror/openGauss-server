8b6f67a07b77aa7aad8a9822315194511d7d17dd
梅程 <517719039@qq.com>
2024-12-13 17:12:11 +0800

    修复subtype PR5804升级问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_003.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_003.sql
@@ -1,10 +1,11 @@
 /*------ add sys fuction subtype_in ------*/
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 9462;
 CREATE FUNCTION pg_catalog.subtype_in(cstring, oid, int4)
-RETURNS anyelement LANGUAGE INTERNAL as 'subtype_in';
-
+RETURNS "any" LANGUAGE INTERNAL as 'subtype_in' STABLE;
+COMMENT ON FUNCTION pg_catalog.subtype_in(cstring, oid, int4) IS 'I/O';
 
 /*------ add sys fuction subtype_recv ------*/
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 9463;
 CREATE FUNCTION pg_catalog.subtype_recv(internal, oid, int4)
-RETURNS anyelement LANGUAGE INTERNAL as 'subtype_recv';
+RETURNS "any" LANGUAGE INTERNAL as 'subtype_recv' STABLE;
+COMMENT ON FUNCTION pg_catalog.subtype_recv(internal, oid, int4) IS 'I/O';
