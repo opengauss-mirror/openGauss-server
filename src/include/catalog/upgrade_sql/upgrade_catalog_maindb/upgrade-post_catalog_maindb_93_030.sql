fca10b6d5424ef7fcb1bf99279f43628a523f2b5
梅程 <517719039@qq.com>
2024-12-19 11:39:51 +0800

    修复query_node_reform_info升级元数据

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_030.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_93_030.sql
@@ -17,3 +17,16 @@ CREATE OR REPLACE FUNCTION pg_catalog.query_node_reform_info
  LANGUAGE internal
  STRICT NOT FENCED NOT SHIPPABLE ROWS 64
 AS $function$query_node_reform_info$function$;
+comment on function pg_catalog.query_node_reform_info
+(
+    OUT reform_node_id          integer,
+    OUT reform_type             text,
+    OUT reform_start_time       timestamp with time zone,
+    OUT reform_end_time         timestamp with time zone,
+    OUT is_reform_success       boolean,
+    OUT redo_start_time         timestamp with time zone,
+    OUT redo_end_time           timestamp with time zone,
+    OUT xlog_total_bytes        int8,
+    OUT hashmap_construct_time  timestamp with time zone,
+    OUT action                  text
+) is 'query node reform information';
