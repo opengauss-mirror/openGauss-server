fe0407d27a4258e77c54da248716da4d357178fe
muyulinzhong <realitylinxuan@163.com>
2024-12-26 15:31:40 +0800

    [修改]因删除buffer置脏导致的initdb流程异常的问题

--- src/gausskernel/storage/smgr/segstore.cpp
+++ src/gausskernel/storage/smgr/segstore.cpp
@@ -1475,6 +1475,7 @@ void seg_extend(SMgrRelation reln, ForkNumber forknum, BlockNumber blocknum, cha
         START_CRIT_SECTION();
         seg_head->nblocks = blocknum + 1;
         SegMarkBufferDirty(seg_buffer);
+        MarkBufferDirty(buf);
 
         XLogBeginInsert();
         XLogRegisterBuffer(0, seg_buffer, REGBUF_KEEP_DATA);
