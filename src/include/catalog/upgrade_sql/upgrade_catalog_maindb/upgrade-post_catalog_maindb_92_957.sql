db220e8b9fb7ac3540d89b1cf13013434d0c2caf
梅程 <517719039@qq.com>
2024-12-27 11:40:14 +0800

    修复升级元数据不一致问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_957.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_957.sql
@@ -55,3 +55,114 @@ CREATE FUNCTION pg_catalog.gs_stat_walrecvwriter(
     out cur_time            timestamptz
 )
 RETURNS SETOF record LANGUAGE INTERNAL ROWS 1 as 'gs_stat_walrecvwriter' stable;
+
+DO $$
+DECLARE
+    ans boolean;
+BEGIN
+select case when count(*)=1 then true else false end from (select * from pg_conversion where conname = 'gb18030_2022_to_utf8' limit 1) into ans;
+    if ans = true THEN
+        comment on conversion gb18030_2022_to_utf8 is 'conversion for GB18030_2022 to UTF8';
+    end if;
+END$$;
+
+DO $$
+DECLARE
+    ans boolean;
+BEGIN
+select case when count(*)=1 then true else false end from (select * from pg_conversion where conname = 'utf8_to_gb18030_2022' limit 1) into ans;
+    if ans = true THEN
+        comment on conversion utf8_to_gb18030_2022 is 'conversion for UTF8 to GB18030_2022';
+    end if;
+END$$;
+
+
+DROP FUNCTION IF EXISTS pg_catalog.gs_hot_standby_space_info() cascade;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 6218;
+CREATE OR REPLACE FUNCTION pg_catalog.gs_hot_standby_space_info
+(  OUT base_page_file_num xid,
+   OUT base_page_total_size xid,
+   OUT lsn_info_meta_file_num xid,
+   OUT lsn_info_meta_total_size xid,
+   OUT block_info_meta_file_num xid,
+   OUT block_info_meta_total_size xid
+   )
+RETURNS SETOF record LANGUAGE INTERNAL ROWS 1 STABLE as 'gs_hot_standby_space_info';
+
+DROP FUNCTION IF EXISTS pg_catalog.btvarstrequalimage(oid) cascade;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 4608;
+CREATE OR REPLACE FUNCTION pg_catalog.btvarstrequalimage("any") RETURNS boolean LANGUAGE INTERNAL IMMUTABLE STRICT AS 'btvarstrequalimage';
+
+DROP FUNCTION IF EXISTS pg_catalog.btequalimage(oid) CASCADE;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 4609;
+CREATE OR REPLACE FUNCTION pg_catalog.btequalimage("any") RETURNS boolean LANGUAGE INTERNAL IMMUTABLE STRICT AS 'btequalimage';
+
+DO $$
+DECLARE
+    ans boolean;
+BEGIN
+select case when count(*)=1 then true else false end from (select nspname from pg_namespace where nspname='coverage' limit 1) into ans;
+    if ans = true THEN
+        DROP table IF EXISTS coverage.proc_coverage;
+        DROP SEQUENCE IF EXISTS coverage.proc_coverage_coverage_id_seq;
+        DROP SCHEMA IF EXISTS coverage cascade;
+    end if;
+END$$;
+
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_NAMESPACE, 4994;
+CREATE SCHEMA coverage;
+COMMENT ON schema coverage IS 'coverage schema';
+
+CREATE SEQUENCE coverage.proc_coverage_coverage_id_seq START 1;
+CREATE unlogged table coverage.proc_coverage(
+	coverage_id bigint NOT NULL DEFAULT nextval('coverage.proc_coverage_coverage_id_seq'::regclass),
+	pro_oid oid NOT NULL,
+	pro_name text NOT NULL,
+	db_name text NOT NULL,
+	pro_querys text NOT NULL,
+	pro_canbreak bool[] NOT NULL,
+	coverage int[] NOT NULL
+) WITH (orientation=row, compression=no);
+REVOKE ALL on table coverage.proc_coverage FROM public;
+
+DROP FUNCTION IF EXISTS pg_catalog.generate_procoverage_report(int8, int8) CASCADE;
+SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 5734;
+CREATE OR REPLACE FUNCTION pg_catalog.generate_procoverage_report(int8, int8) RETURNS text
+LANGUAGE INTERNAL STRICT as 'generate_procoverage_report';
+
+-- add this. dropped by 92_906
+DO $gs_slow_query_info$
+DECLARE
+    ans boolean;
+BEGIN
+select case when count(*)=1 then true else false end from (select relname from pg_class where relname='gs_slow_query_info' and relnamespace=4988 limit 1) into ans;
+    if ans = true THEN
+        CREATE OR REPLACE FUNCTION dbe_perf.global_slow_query_info_bytime(text, TIMESTAMP, TIMESTAMP, int)
+        RETURNS setof dbe_perf.gs_slow_query_info
+        AS $$
+DECLARE
+                row_data dbe_perf.gs_slow_query_info%rowtype;
+                row_name record;
+                query_str text;
+                query_str_nodes text;
+                query_str_cn text;
+                BEGIN
+                                IF $1 IN ('start_time', 'finish_time') THEN
+                                ELSE
+                                         raise WARNING 'Illegal character entered for function, colname must be start_time or finish_time';
+                                        return;
+                                END IF;
+                                --Get all the node names
+                                query_str_nodes := 'SELECT node_name FROM pgxc_node WHERE node_type=''C'' AND nodeis_active = true';
+                                query_str_cn := 'SELECT * FROM dbe_perf.gs_slow_query_info where '||$1||'>'''''||$2||''''' and '||$1||'<'''''||$3||''''' limit '||$4;
+                                FOR row_name IN EXECUTE(query_str_nodes) LOOP
+                                                query_str := 'EXECUTE DIRECT ON (' || row_name.node_name || ') ''' || query_str_cn||''';';
+                                                FOR row_data IN EXECUTE(query_str) LOOP
+                                                                return next row_data;
+                                                END LOOP;
+                                END LOOP;
+                                return;
+                END; $$
+        LANGUAGE 'plpgsql' NOT FENCED;
+    end if;
+END$gs_slow_query_info$;
