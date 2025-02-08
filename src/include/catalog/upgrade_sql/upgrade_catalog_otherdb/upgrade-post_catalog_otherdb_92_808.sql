08ffbfb44d123cd8e228c29d7bec9ad4b75a2c67
maxiang <maxiang9527@126.com>
2025-02-08 09:50:37 +0000

    修改comment信息
    Signed-off-by: maxiang <maxiang9527@126.com>

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_808.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_92_808.sql
@@ -9,11 +9,13 @@ BEGIN
          RETURNS int16
          LANGUAGE internal
          STABLE NOT FENCED NOT SHIPPABLE AS 'last_insert_id_no_args';
+         comment on FUNCTION pg_catalog.last_insert_id() is 'the first automatically generated value successfully inserted for an AUTO_INCREMENT column';
          
         SET LOCAL inplace_upgrade_next_system_object_oids=IUO_PROC, 4079;
         CREATE FUNCTION pg_catalog.last_insert_id(int16)
          RETURNS int16
          LANGUAGE internal
          IMMUTABLE NOT FENCED NOT SHIPPABLE AS 'last_insert_id';
+         comment on FUNCTION pg_catalog.last_insert_id(int16) is 'the next value to be returned by last_insert_id()';
     end if;
 END$$;
