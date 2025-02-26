003e753fad5d23d866bd22356c7bc6eb12c23535
nnuanyang <zhaoxl@vastdata.com.cn>
2025-02-26 14:27:08 +0800

    操作符元数据不一致

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_918.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_918.sql
@@ -31,11 +31,12 @@ jsonb, text[]
 DROP OPERATOR IF EXISTS pg_catalog.-(jsonb, text) CASCADE;
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_GENERAL, 3252;
 CREATE OPERATOR pg_catalog.-(LEFTARG = jsonb, RIGHTARG = text, PROCEDURE = jsonb_delete);
-
+COMMENT ON OPERATOR pg_catalog.->(jsonb, text) IS 'delete text';
 DROP OPERATOR IF EXISTS pg_catalog.-(jsonb, int) CASCADE;
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_GENERAL, 3251;
 CREATE OPERATOR pg_catalog.-(LEFTARG = jsonb, RIGHTARG = int, PROCEDURE = jsonb_delete);
-
+COMMENT ON OPERATOR pg_catalog.-(jsonb, int) IS 'delete index';
 DROP OPERATOR IF EXISTS pg_catalog.-(jsonb, text[]) CASCADE;
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_GENERAL, 3253;
 CREATE OPERATOR pg_catalog.-(LEFTARG = jsonb, RIGHTARG = text[], PROCEDURE = jsonb_delete);
+COMMENT ON OPERATOR pg_catalog.-(jsonb, text[]) IS 'delete text[]';
