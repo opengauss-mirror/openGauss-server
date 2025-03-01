798fe23e4f6260d1a8e06f2eefb3aa9bc9b0ae39
chenxiaobin19 <1025221611@qq.com>
2025-03-01 15:28:19 +0800

    修复普通用户通过函数非法提权的问题

--- src/common/pl/plpgsql/src/pl_handler.cpp
+++ src/common/pl/plpgsql/src/pl_handler.cpp
@@ -19,7 +19,6 @@
 #include "utils/fmgroids.h"
 #include "utils/pl_package.h"
 #include "auditfuncs.h"
-#include "catalog/pg_cast.h"
 #include "catalog/pg_proc.h"
 #include "catalog/pg_type.h"
 #include "catalog/gs_package.h"
@@ -785,10 +784,6 @@ Datum plpgsql_call_handler(PG_FUNCTION_ARGS)
     int rc;
     Oid func_oid = fcinfo->flinfo->fn_oid;
     Oid* saved_Pseudo_CurrentUserId = NULL;
-    Oid old_user = InvalidOid;
-    int save_sec_context = 0;
-    Oid cast_owner = InvalidOid;
-    bool has_switch = false;
     PGSTAT_INIT_TIME_RECORD();
     bool needRecord = false;
     PLpgSQL_package* pkg = NULL;
@@ -805,14 +800,6 @@ Datum plpgsql_call_handler(PG_FUNCTION_ARGS)
         nonatomic = IsA(fcinfo->context, FunctionScanState) && !castNode(FunctionScanState, fcinfo->context)->atomic;
     } else {
         nonatomic = u_sess->SPI_cxt.is_allow_commit_rollback;
-    }
-
-    /* get cast owner and make sure current user is cast owner when execute cast-func */
-    GetUserIdAndSecContext(&old_user, &save_sec_context);
-    cast_owner = u_sess->exec_cxt.cast_owner;
-    if (cast_owner != InvalidCastOwnerId && OidIsValid(cast_owner)) {
-        SetUserIdAndSecContext(cast_owner, save_sec_context | SECURITY_LOCAL_USERID_CHANGE);
-        has_switch = true;
     }
 
     bool save_need_create_depend = u_sess->plsql_cxt.need_create_depend;
@@ -1163,10 +1150,6 @@ Datum plpgsql_call_handler(PG_FUNCTION_ARGS)
     }
 #endif
     UpdateCurrCompilePgObjStatus(save_curr_status);
-    if (has_switch) {
-        SetUserIdAndSecContext(old_user, save_sec_context);
-        u_sess->exec_cxt.cast_owner = InvalidOid;
-    }
     return retval;
 }
 void CheckPipelinedWithOutParam(const PLpgSQL_function *func)
