cbfbbd533f623207caaaceeb32046aa0e418056c
wenger <pan17@21cn.com>
2025-03-03 12:19:07 +0800

    修复pubddl_decode函数升级前后元数据不一致问题

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 93040;
+const uint32 GRAND_VERSION_NUM = 93041;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
