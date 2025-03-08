adf1e3aef23ce7fd29dc002e43089b4dcc6f5b36
gentle_hu <gentle_hu@qq.com>
2025-03-08 16:53:57 +0800

    check backend exist and clean when switchover

--- src/gausskernel/process/postmaster/postmaster.cpp
+++ src/gausskernel/process/postmaster/postmaster.cpp
@@ -8290,6 +8290,26 @@ static const char* GetProcName(ThreadId pid)
     }
 }
 
+
+/*
+ * clear child in backend list who is already dead in some case like switchover.
+ */
+static void CheckChildExistOrClean(List* backendList)
+{
+    ListCell* cell = NULL;
+
+    foreach (cell, backendList) {
+        Backend* bp = (Backend*)lfirst(cell);
+
+        if (pthread_kill(bp->pid, 0) == ESRCH) {
+            CleanupBackend(bp->pid, 0);
+        } else {
+            ereport(WARNING, (errmsg("Cannot clear backend(%ld) still alive in backend_list.", bp->pid)));
+        }
+    }
+}
+
+
 /*
  * CleanupBackend -- cleanup after terminated backend.
  *
@@ -9094,7 +9114,7 @@ static void PostmasterStateMachine(void)
     }
 }
 
-void signalBackend(Backend* bn, int signal, int be_mode)
+bool signalBackend(Backend* bn, int signal, int be_mode)
 {
     if ((uint32)bn->flag & THRD_EXIT)
         ereport(LOG, (errmsg("Thread pid(%lu), flag(%d) may be exited repeatedly", bn->pid, bn->flag)));
@@ -9116,7 +9136,10 @@ void signalBackend(Backend* bn, int signal, int be_mode)
                 g_instance.demotion,
                 Shutdown,
                 be_mode)));
+
+        return false;
     }
+    return true;
 }
 
 /*
@@ -9163,6 +9186,7 @@ void signal_child(ThreadId pid, int signal, int be_mode)
 static bool SignalSpecialChildren(int signal, int target, ThreadId pid) {
     Dlelem* curr = NULL;
     bool signaled = false;
+    List* failedList = NIL;
 
     for (curr = DLGetHead(g_instance.backend_list); curr; curr = DLGetSucc(curr)) {
         Backend* bp = (Backend*)DLE_VAL(curr);
@@ -9203,8 +9227,16 @@ static bool SignalSpecialChildren(int signal, int target, ThreadId pid) {
 
         ereport(DEBUG4, (errmsg_internal("sending signal %d to process %lu", signal, bp->pid)));
 
-        signalBackend(bp, signal, child);
+        bool res = signalBackend(bp, signal, child);
         signaled = true;
+        if (!res && signal == SIGTERM && pmState == PM_WAIT_BACKENDS) {
+            failedList = lappend(failedList, bp);
+        }
+    }
+
+    if (failedList != NIL) {
+        CheckChildExistOrClean(failedList);
+        list_free(failedList);
     }
 
     return signaled;
