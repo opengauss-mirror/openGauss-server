c0d640944f3f78ac8fe1f2a211608f43de743fcb
梅程 <517719039@qq.com>
2025-04-24 17:48:38 +0800

    修复db4ai升级元数据问题

--- src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_93_044.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_otherdb/upgrade-post_catalog_otherdb_93_044.sql
@@ -28,3 +28,49 @@ comment on function pg_catalog.query_parameterization_views(
     out parameterized_query text
 ) is 'query_parameterization_views';
 SET LOCAL inplace_upgrade_next_system_object_oids = IUO_PROC, 0;
+
+ALTER FUNCTION db4ai.create_snapshot(
+    IN i_schema NAME,
+    IN i_name NAME,
+    IN i_commands TEXT[],
+    IN i_vers NAME,
+    IN i_comment TEXT
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.prepare_snapshot(
+    IN i_schema NAME,
+    IN i_parent NAME,
+    IN i_commands TEXT[],
+    IN i_vers NAME,
+    IN i_comment TEXT
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.manage_snapshot_internal(
+    IN i_schema NAME,
+    IN i_name NAME,
+    IN publish BOOLEAN
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.archive_snapshot(
+    IN i_schema NAME,
+    IN i_name NAME
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.publish_snapshot(
+    IN i_schema NAME,
+    IN i_name NAME
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.purge_snapshot(
+    IN i_schema NAME,
+    IN i_name NAME
+) SET client_min_messages TO ERROR;
+
+ALTER FUNCTION db4ai.sample_snapshot(
+    IN i_schema NAME,
+    IN i_parent NAME,
+    IN i_sample_infixes NAME[],
+    IN i_sample_ratios NUMBER[],
+    IN i_stratify NAME[],
+    IN i_sample_comments TEXT[]
+) SET client_min_messages TO ERROR;
