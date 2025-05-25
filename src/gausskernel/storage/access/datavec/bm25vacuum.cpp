c4681cea2d108511119b296c53a996c4fd3cb0d9
wlff234 <wulf234@163.com>
2025-05-25 21:32:45 +0800

    vacuum for invertedList

--- src/gausskernel/storage/access/datavec/bm25vacuum.cpp
+++ src/gausskernel/storage/access/datavec/bm25vacuum.cpp
@@ -1,3 +1,25 @@
+/*
+* Copyright (c) 2025 Huawei Technologies Co.,Ltd.
+ *
+ * openGauss is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ * -------------------------------------------------------------------------
+ *
+ * bm25vacuum.cpp
+ *
+ * IDENTIFICATION
+ *        src/gausskernel/storage/access/datavec/bm25vacuum.cpp
+ *
+ * -------------------------------------------------------------------------
+ */
 
 #include <unordered_map>
 #include "postgres.h"
@@ -26,8 +48,8 @@ static BlockNumber FindForwrdBlock(Relation index, BlockNumber startBlkno, uint6
     return startBlkno;
 }
 
-static void MarkDeleteDocuments(Relation index, IndexBulkDeleteCallback callback, void *callbackState, Vector<BM25DocumentItem> &deleteDocs,
-    BM25EntryPages &entryPages, BufferAccessStrategy &bas)
+static void MarkDeleteDocuments(Relation index, IndexBulkDeleteCallback callback, void *callbackState,
+    Vector<BM25DocumentItem> &deleteDocs, BM25EntryPages &entryPages, BufferAccessStrategy &bas)
 {
     Page page;
     Buffer buf;
@@ -67,8 +89,8 @@ static void MarkDeleteDocuments(Relation index, IndexBulkDeleteCallback callback
     }
 }
 
-static void FindDocumetTokens(Relation index, Vector<BM25DocumentItem> &deleteDocs, unordered_map<uint32, DeleteToken> deleteTokens,
-    BM25EntryPages &entryPages, BufferAccessStrategy &bas)
+static void FindDocumetTokens(Relation index, Vector<BM25DocumentItem> &deleteDocs,
+    unordered_map<uint32, DeleteToken> &deleteTokens, BM25EntryPages &entryPages, BufferAccessStrategy &bas)
 {
     Page page;
     Buffer buf;
@@ -100,8 +122,8 @@ static void FindDocumetTokens(Relation index, Vector<BM25DocumentItem> &deleteDo
             }
             cpage = BufferGetPage(cbuf);
             uint16 offset = startToken % BM25_DOC_FORWARD_MAX_COUNT_IN_PAGE;
-            BM25DocForwardItem *forwardItem =
-                (BM25DocForwardItem*)((char *)cpage + sizeof(PageHeaderData) + offset * BM25_DOCUMENT_FORWARD_ITEM_SIZE);
+            BM25DocForwardItem *forwardItem = (BM25DocForwardItem*)(
+                (char *)cpage + sizeof(PageHeaderData) + offset * BM25_DOCUMENT_FORWARD_ITEM_SIZE);
             if (deleteTokens.find(forwardItem->tokenId) == deleteTokens.end()) {
                 DeleteToken deleteToken;
                 deleteToken.tokenHash = forwardItem->tokenHash;
@@ -117,9 +139,142 @@ static void FindDocumetTokens(Relation index, Vector<BM25DocumentItem> &deleteDo
     }
 }
 
-static void DeleteTokensFromPostinglist(Relation index, unordered_map<uint32, DeleteToken> deleteTokens, BM25EntryPages &entryPages)
+static bool FindDocId(DeleteToken &deleteToken, uint32 docId)
 {
+    for (size_t idx = 0; idx < deleteToken.docIds.size(); ++idx) {
+        if (deleteToken.docIds[idx] == docId) {
+            return true;
+        }
+    }
+    return false;
+}
 
+static void BM25UpdateTokenMeta(Relation index, uint32 deletedDocCount, BlockNumber tokenMetaBlkno,
+    OffsetNumber tokenMetaOffno, BlockNumber insertPage)
+{
+    Buffer buf;
+    Page page;
+    GenericXLogState *state = nullptr;
+
+    buf = ReadBuffer(index, tokenMetaBlkno);
+    LockBuffer(buf, BUFFER_LOCK_EXCLUSIVE);
+    state = GenericXLogStart(index);
+    page = GenericXLogRegisterBuffer(state, buf, GENERIC_XLOG_FULL_IMAGE);
+    BM25TokenMetaPage tokenMeta = (BM25TokenMetaPage)PageGetItem(page, PageGetItemId(page, tokenMetaOffno));
+    if (BlockNumberIsValid(insertPage) && insertPage < tokenMeta->lastInsertBlkno) {
+        tokenMeta->lastInsertBlkno =  insertPage;
+    }
+    tokenMeta->docCount -= deletedDocCount;
+    GenericXLogFinish(state);
+    UnlockReleaseBuffer(buf);
+    return;
+}
+
+static void VacuumInvertedList(Relation index, uint32 tokenId, DeleteToken &deleteToken)
+{
+    BM25MetaPageData meta;
+    BM25GetMetaPageInfo(index, &meta);
+    BlockNumber hashBucketsBlkno = meta.entryPageList.hashBucketsPage;
+    uint32 maxHashBucketCount = meta.entryPageList.maxHashBucketCount;
+
+    /* bucket location */
+    uint32 bucketIdx = deleteToken.tokenHash % (maxHashBucketCount * BM25_BUCKET_PAGE_ITEM_SIZE);
+    Buffer cHashBucketsbuf = ReadBuffer(index, hashBucketsBlkno);
+    LockBuffer(cHashBucketsbuf, BUFFER_LOCK_SHARE);
+    Page cHashBucketspage = BufferGetPage(cHashBucketsbuf);
+    BM25HashBucketPage bucketInfo = (BM25HashBucketPage)PageGetItem(cHashBucketspage,
+        PageGetItemId(cHashBucketspage, (bucketIdx / BM25_BUCKET_PAGE_ITEM_SIZE) + 1));
+    BlockNumber tokenMetasBlkno = bucketInfo->bucketBlkno[bucketIdx % BM25_BUCKET_PAGE_ITEM_SIZE];
+    UnlockReleaseBuffer(cHashBucketsbuf);
+
+    /* find tokenMeta */
+    if (!BlockNumberIsValid(tokenMetasBlkno)) {
+        return;
+    }
+    BlockNumber postingBlkno = InvalidBlockNumber;
+    BlockNumber tokenMetaBlkno = InvalidBlockNumber;
+    OffsetNumber tokenMetaOffno = InvalidOffsetNumber;
+    Buffer cTokenMetasbuf;
+    Page cTokenMetaspage;
+    BlockNumber nextTokenMetasBlkno = tokenMetasBlkno;
+    while (BlockNumberIsValid(nextTokenMetasBlkno)) {
+        cTokenMetasbuf = ReadBuffer(index, nextTokenMetasBlkno);
+        LockBuffer(cTokenMetasbuf, BUFFER_LOCK_SHARE);
+        cTokenMetaspage = BufferGetPage(cTokenMetasbuf);
+        OffsetNumber maxoffno = PageGetMaxOffsetNumber(cTokenMetaspage);
+        for (OffsetNumber offnoTokenMeta = FirstOffsetNumber; offnoTokenMeta <= maxoffno; offnoTokenMeta++) {
+            BM25TokenMetaPage tokenMeta =
+                (BM25TokenMetaPage)PageGetItem(cTokenMetaspage, PageGetItemId(cTokenMetaspage, offnoTokenMeta));
+            if ((tokenMeta->hashValue == deleteToken.tokenHash) && (tokenMeta->tokenId == tokenId)) {
+                postingBlkno = tokenMeta->postingBlkno;
+                tokenMetaBlkno = nextTokenMetasBlkno;
+                tokenMetaOffno = offnoTokenMeta;
+                break;
+            }
+        }
+        nextTokenMetasBlkno = BM25PageGetOpaque(cTokenMetaspage)->nextblkno;
+        UnlockReleaseBuffer(cTokenMetasbuf);
+    }
+
+    /* process posting list, delete items in docId list */
+    if (!BlockNumberIsValid(postingBlkno)) {
+        return;
+    }
+
+    BufferAccessStrategy bas = GetAccessStrategy(BAS_BULKREAD);
+    BlockNumber nextPostingBlkno = postingBlkno;
+    BlockNumber insertPage = InvalidBlockNumber;
+    uint32 deletedDocCount = 0;
+    while (BlockNumberIsValid(nextPostingBlkno)) {
+        Buffer postingListbuf;
+        Page postingListpage;
+        GenericXLogState *postingState = nullptr;
+        OffsetNumber deletable[MaxOffsetNumber];
+        int ndeletable;
+
+        vacuum_delay_point();
+
+        postingListbuf = ReadBufferExtended(index, MAIN_FORKNUM, nextPostingBlkno, RBM_NORMAL, bas);
+        LockBufferForCleanup(postingListbuf);
+        BM25GetPage(index, &postingListpage, postingListbuf, &postingState, false);
+        OffsetNumber maxoffno = PageGetMaxOffsetNumber(postingListpage);
+        ndeletable = 0;
+        for (OffsetNumber offnoPosting = FirstOffsetNumber; offnoPosting <= maxoffno; offnoPosting++) {
+            BM25TokenPostingPage postingItem =
+                (BM25TokenPostingPage)PageGetItem(postingListpage, PageGetItemId(postingListpage, offnoPosting));
+            if (FindDocId(deleteToken, postingItem->docId)) {
+                deletable[ndeletable++] = offnoPosting;
+                deletedDocCount++;
+                break;
+            }
+        }
+        if (!BlockNumberIsValid(insertPage) && ndeletable > 0) {
+            insertPage = nextPostingBlkno;
+        }
+        nextPostingBlkno = BM25PageGetOpaque(cTokenMetaspage)->nextblkno;
+        if (ndeletable > 0) {
+            /* Delete item */
+            PageIndexMultiDelete(postingListpage, deletable, ndeletable);
+            GenericXLogFinish(postingState);
+        } else {
+            GenericXLogAbort(postingState);
+        }
+        UnlockReleaseBuffer(postingListbuf);
+    }
+    if (BlockNumberIsValid(insertPage)) {
+        BM25UpdateTokenMeta(index, deletedDocCount, tokenMetaBlkno, tokenMetaOffno, insertPage);
+    }
+    FreeAccessStrategy(bas);
+    return;
+}
+
+
+static void DeleteTokensFromPostinglist(Relation index, unordered_map<uint32, DeleteToken> deleteTokens,
+    BM25EntryPages &entryPages)
+{
+    for (auto iter = deleteTokens.begin(); iter != deleteTokens.end(); iter++) {
+        VacuumInvertedList(index, iter->first, iter->second);
+    }
 }
 
 static void UpdateBM25Statistics(Relation index, Vector<BM25DocumentItem> &deleteDocs, BM25EntryPages &entryPages)
@@ -199,8 +354,9 @@ IndexBulkDeleteResult *bm25vacuumcleanup_internal(IndexVacuumInfo *info, IndexBu
 
     /* stats is NULL if ambulkdelete not called */
     /* OK to return NULL if index not changed */
-    if (stats == NULL)
+    if (stats == NULL) {
         return NULL;
+    }
 
     stats->num_pages = RelationGetNumberOfBlocks(rel);
 
