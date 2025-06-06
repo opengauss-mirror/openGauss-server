9c0aaa0d642629a086a162cf477be93691153e2e
congzhou2603 <zhoucong42@huawei.com>
2025-06-06 09:27:34 +0800

    Revert "优化DSS拷贝文件 + 修改合并冲突"
    This reverts commit 2f313a962a8483da97436e3fe41cec39610a8798.

--- src/common/backend/utils/init/miscinit.cpp
+++ src/common/backend/utils/init/miscinit.cpp
@@ -2150,6 +2150,8 @@ void initDssPath(char *dssdir)
     rc = snprintf_s(g_instance.datadir_cxt.hbaConfigFilePath, MAXPGPATH, MAXPGPATH - 1, "%s/shared_pg_hba.conf",
         dssdir);
     securec_check_ss(rc, "", "");
+
+    ss_initdwsubdir(dssdir);
 }
 
 void initDSSConf(void)
