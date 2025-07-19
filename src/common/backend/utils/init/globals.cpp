eef9f2b44523c61b6b70ac172d49cf0d6ffe73c2
wangfeihuo <wangfeihuo@huawei.com>
2025-07-19 15:29:59 +0800

    增加与m兼容的information_schema.engines表

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 93056;
+const uint32 GRAND_VERSION_NUM = 93057;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
