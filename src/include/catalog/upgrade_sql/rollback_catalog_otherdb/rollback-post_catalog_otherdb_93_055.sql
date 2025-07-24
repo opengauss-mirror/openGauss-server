dc0ddc392073fbb838234bc421100cfb5ec817ec
bomou <paakmau@outlook.com>
2025-07-24 09:59:47 +0800

    修复 B 库升级 to_char 函数冲突问题
    Co-authored-by: He Bomou<paakmau@outlook.com>

--- src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_93_055.sql
+++ src/include/catalog/upgrade_sql/rollback_catalog_otherdb/rollback-post_catalog_otherdb_93_055.sql
@@ -1,2 +1,14 @@
-DROP FUNCTION IF EXISTS pg_catalog.to_char(time, text) CASCADE;
+do $$
+DECLARE
+ans boolean;
+BEGIN
+    for ans in select case when count(*)=1 then true else false end as ans from (select extname from pg_extension where extname='dolphin')
+    LOOP
+        if ans = false then
+            DROP FUNCTION IF EXISTS pg_catalog.to_char(time, text) CASCADE;
+        end if;
+        exit;
+    END LOOP;
+END$$;
+
 DROP FUNCTION IF EXISTS pg_catalog.to_char(timetz, text) CASCADE;
