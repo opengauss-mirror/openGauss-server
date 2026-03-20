742f05e7805a3662828a12922f0c2d4c80b840f2
wofanzheng <2399541971@qq.com>
2026-03-20 10:31:35 +0800

    l2_norm支持vector数据类型
    (cherry picked from commit f492d68b3de13f8e71cccd1d9aeea06a9177b36e)

--- src/common/backend/utils/init/globals.cpp
+++ src/common/backend/utils/init/globals.cpp
@@ -77,7 +77,7 @@ bool will_shutdown = false;
  *
  ********************************************/
 
-const uint32 GRAND_VERSION_NUM = 93090;
+const uint32 GRAND_VERSION_NUM = 93091;
 
 /********************************************
  * 2.VERSION NUM FOR EACH FEATURE
