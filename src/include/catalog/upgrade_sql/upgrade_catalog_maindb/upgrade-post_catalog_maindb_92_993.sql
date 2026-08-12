84d52698b11ddfc52655b4e78eb56efb45db701a
shijuzheng1997 <shijuzheng1997@126.com>
2026-08-12 10:53:58 +0800

    remove garbage pg_conversion info in pg_description

--- src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_993.sql
+++ src/include/catalog/upgrade_sql/upgrade_catalog_maindb/upgrade-post_catalog_maindb_92_993.sql
@@ -7,3 +7,7 @@ STABLE STRICT NOT FENCED NOT SHIPPABLE
 AS $function$ss_ub_link_available$function$;
 
 COMMENT ON FUNCTION pg_catalog.ss_ub_link_available() IS 'whether the UB cache link is currently available';
+
+-- Drop pg_description rows whose pg_conversion target no longer exists.
+delete from pg_catalog.pg_description where classoid = 2607 and objsubid = 0
+    and objoid not in (select oid from pg_catalog.pg_conversion);
